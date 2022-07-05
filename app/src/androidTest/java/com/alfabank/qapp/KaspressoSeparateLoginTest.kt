5de1a045c3769af40708990a65c889f9879d6f85
will-see <petrov.dmi3@gmail.com>
2022-07-05 15:22:25 +0300

    Login tests done

---
+++ app/src/androidTest/java/com/alfabank/qapp/KaspressoSeparateLoginTest.kt
@@ -0,0 +1,143 @@
+package com.alfabank.qapp
+
+import android.Manifest
+import android.view.View
+import android.widget.EditText
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.rule.GrantPermissionRule
+import com.alfabank.qapp.presentation.MainActivity
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import io.github.kakaocup.kakao.edit.EditableActions
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class KaspressoSeparateLoginTest : TestCase() {
+
+    @get:Rule
+    val runtimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
+    @get:Rule
+    val activityRule = activityScenarioRule<MainActivity>()
+
+        @Test
+    fun emptyTest() {
+        run {
+            step("Empy fields test") {
+                LoginScreen {
+                    btnConfirm {
+                        click()
+                    }
+                    tvError {
+                        hasAnyText()
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun wrongLoginTest() {
+        run {
+            step("Wrong login test") {
+                LoginScreen {
+                    etLogin {
+                        typeText("wrongLogin")
+                    }
+                    etPass {
+                        typeText("Password")
+                    }
+                    btnConfirm {
+                        click()
+                    }
+                    tvError {
+                        hasAnyText()
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun wrongPassTest() {
+        run {
+            step("Wrong pass test") {
+                LoginScreen {
+                    etLogin {
+                        typeText("Login")
+                    }
+                    etPass {
+                        typeText("wrongPassword")
+                    }
+                    btnConfirm {
+                        click()
+                    }
+                    tvError {
+                        hasAnyText()
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun wrongLoginPassTest() {
+        run {
+            step("Wrong login & pass test") {
+                LoginScreen {
+                    etLogin {
+                        typeText("wrongLogin")
+                    }
+                    etPass {
+                        typeText("wrongPassword")
+                    }
+                    btnConfirm {
+                        click()
+                    }
+                    tvError {
+                        hasAnyText()
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun validTest() {
+        run {
+            step("Valid login & pass test") {
+                LoginScreen {
+                    etLogin {
+                        typeText("Login")
+                    }
+                    etPass {
+                        typeText("Password")
+                    }
+                    btnConfirm {
+                        click()
+                    }
+                    tvError {
+                        hasEmptyText()
+                    }
+
+                }
+            }
+            step("Loader test") {
+                LoginScreen {
+                    stBar {
+                        isVisible()
+                        isEnabled()
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/alfabank/qapp/LoginScreen.kt
@@ -0,0 +1,20 @@
+package com.alfabank.qapp
+
+import com.alfabank.qapp.presentation.MainActivity
+import com.kaspersky.kaspresso.screens.KScreen
+import io.github.kakaocup.kakao.common.views.KView
+import io.github.kakaocup.kakao.edit.KEditText
+import io.github.kakaocup.kakao.text.KButton
+import io.github.kakaocup.kakao.text.KTextView
+
+object LoginScreen : KScreen<LoginScreen>(){
+    override val layoutId: Int?= R.layout.fragment_login
+    override val viewClass: Class<*>?=MainActivity::class.java
+
+    val tvTitle = KTextView{withId(R.id.tvTitle)}
+    val etLogin = KEditText{withId(R.id.etUsername)}
+    val etPass = KEditText{withId(R.id.etPassword)}
+    val btnConfirm = KButton{withId(R.id.btnConfirm)}
+    val tvError = KTextView{withId(R.id.tvError)}
+    val stBar = KView{withId(R.id.loader)}
+}
